a05ce873a7cb256f9d7afb83a395ae104bfe22bd
SongNoin <as5427072@gmail.com>
2023-04-04 23:28:25 +0900

    feat 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
+
 function App() {
-  return <div>메인</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
