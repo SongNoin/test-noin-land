8a341da126b9c69fa82951ca2a320202cc02ac17
SongNoin <as5427072@gmail.com>
2023-04-20 01:31:34 +0900

    feat 상식퀴즈 기초 퍼블리싱

--- src/pages/CommonQuiz/index.tsx
+++ src/pages/CommonQuiz/index.tsx
@@ -1,3 +1,36 @@
 export const CommonQuiz = () => {
-  return <div>상식퀴즈</div>;
+  const answerExample = [
+    '가나다라마바사아자차카타파하가나다라마바사아자차카타파',
+    '2번',
+    '3번',
+    '4번',
+  ];
+  return (
+    <div className='mt-6 md:max-w-2xl'>
+      <div className='text-2xl'>
+        1. 문제 제목 문제 제목 문제 제목 문제 제목 문제 제목 문제 제목 문제 제목
+        문제 제목 문제 제목 문제 제목 문제 제목 문제 제목 문제 제목 문제 제목
+      </div>
+      <img
+        className='object-contain mt-3 overflow-hidden rounded-md'
+        alt='quiz image'
+        src='https://post-phinf.pstatic.net/MjAyMDAzMDVfMzAw/MDAxNTgzNDEzNTUyNTY1.0fmKJxIpFxmQxagjwEWOuhaqjS0tMcRZhxhsX_IE-KEg.5NjMNR8OD9KZvaLGvhGfvqECleCQ1sb95k0O6B90bnsg.JPEG/%EC%BF%BC%EC%B9%B48.jpg?type=w1200'
+      />
+      <div className='grid grid-cols-2 gap-1 mt-12 md:grid-cols-4'>
+        {answerExample.map((el) => (
+          <div
+            className='relative border-2 border-blue-300 h-fit pb-[100%] cursor-pointer rounded-md hover:bg-blue-300 md:pb-[50%]'
+            key={el}
+          >
+            <div className='absolute top-0 left-0 w-[100%] h-[100%]'>
+              <div className='flex items-center justify-center h-[100%] p-5'>
+                {el}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='mt-10'></div>
+    </div>
+  );
 };
